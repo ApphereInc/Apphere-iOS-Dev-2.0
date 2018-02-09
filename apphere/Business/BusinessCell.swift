e35bb5e39a4eee2ac93a47988f8766f5a5bc5136
Tony Mann <thephatmann@gmail.com>
2018-02-09 06:40:13 -0800

    Tweak label names

--- apphere/Business/BusinessCell.swift
+++ apphere/Business/BusinessCell.swift
@@ -11,17 +11,15 @@ import UIKit
 class BusinessCell: UICollectionViewCell {
     var business: Business! {
         didSet {
-            photoView.image             = business.photo
-            nameLabel.text              = business.name.uppercased()
-            promotionLabel.text         = business.promotion.uppercased()
-            footerLargeTextLabel.text   = String(business.activeCustomerCount)
-            footerSmallTextLabel.text   = "HERE NOW"
+            photoView.image                 = business.photo
+            nameLabel.text                  = business.name.uppercased()
+            promotionLabel.text             = business.promotion.uppercased()
+            activeCustomerCountLabel.text   = String(business.activeCustomerCount)
         }
     }
     
     @IBOutlet weak var photoView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var promotionLabel: UILabel!
-    @IBOutlet weak var footerLargeTextLabel: UILabel!
-    @IBOutlet weak var footerSmallTextLabel: UILabel!
+    @IBOutlet weak var activeCustomerCountLabel: UILabel!
 }
