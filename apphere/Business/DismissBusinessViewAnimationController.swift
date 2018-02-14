5a3c9f35f5801284401ec877f535337fad24f6d0
Tony Mann <thephatmann@gmail.com>
2018-02-14 15:06:56 -0800

    Remove test change

--- apphere/Business/DismissBusinessViewAnimationController.swift
+++ apphere/Business/DismissBusinessViewAnimationController.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 
-/* Hey Derek!!! */
-
 // Adapted from https://github.com/phillfarrugia/appstore-clone
 
 class DismissBusinessViewAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
