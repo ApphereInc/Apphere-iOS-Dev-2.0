852b255e4a110007b36722fd342ecce9b0d74a53
Tony Mann <thephatmann@gmail.com>
2018-02-17 14:11:15 -0800

    Fade in status bar as list view reappears

--- apphere/Business/DismissBusinessViewAnimationController.swift
+++ apphere/Business/DismissBusinessViewAnimationController.swift
@@ -15,8 +15,8 @@ class DismissBusinessViewAnimationController: NSObject, UIViewControllerAnimated
     
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         guard let businessDetailViewController = transitionContext.viewController(forKey: .from) as? BusinessDetailViewController,
-              let tabBarController = transitionContext.viewController(forKey: .to) as? UITabBarController
-              //let businessListViewController = tabBarController.viewControllers?.first as? BusinessListViewController
+              let tabBarController = transitionContext.viewController(forKey: .to) as? UITabBarController,
+              let businessListViewController = tabBarController.viewControllers?.first as? BusinessListViewController
         else {
             return
         }
@@ -24,6 +24,7 @@ class DismissBusinessViewAnimationController: NSObject, UIViewControllerAnimated
         let cellFrame = businessDetailViewController.view.convert(selectedBusinessCellFrameInWindow, from: nil)
         transitionContext.containerView.insertSubview(tabBarController.view, at: 0)
         businessDetailViewController.view.backgroundColor = UIColor.clear
+        businessListViewController.isStatusBarHidden = true
         
         UIView.animate(withDuration: transitionDuration(using: transitionContext), delay: 0, options: .curveEaseInOut, animations: {
             businessDetailViewController.containerTopConstraint.constant = cellFrame.minY
@@ -32,6 +33,8 @@ class DismissBusinessViewAnimationController: NSObject, UIViewControllerAnimated
             businessDetailViewController.closeButton.alpha = 0
             businessDetailViewController.photoView.layer.cornerRadius = 14.0
             businessDetailViewController.view.layoutIfNeeded()
+            businessListViewController.isStatusBarHidden = false
+            businessListViewController.setNeedsStatusBarAppearanceUpdate()
         }, completion: { (_) in
             transitionContext.completeTransition(!transitionContext.transitionWasCancelled)
         })
