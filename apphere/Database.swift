338eaac1b6c88b0509f63045e570110d915f9f92
Tony Mann <thephatmann@gmail.com>
2018-03-12 07:36:09 -0700

    add event call

--- apphere/Database.swift
+++ apphere/Database.swift
@@ -8,10 +8,10 @@
 
 import Foundation
 import Firebase
+import CodableFirebase
 
 class Database {
     static var shared = Database()
-    lazy var db = Firestore.firestore()
     
     struct Event: Codable {
         enum EventType: Int, Codable {
@@ -21,17 +21,56 @@ class Database {
         
         let type: EventType
         let date: Date
+        let businessId: String
     }
     
     struct Business: Codable {
-        let id: Int
-        var activeCustomerCount: Int
+        var activeCustomerCount: Int = 0
         
-        enum Fields: String, CodingKey {
-            case id = "id"
+        enum CodingKeys: String, CodingKey {
             case activeCustomerCount = "active_customer_count"
         }
     }
+    
+    func add(event: Event, completion: @escaping (Any?, Error?) -> Void) {
+        db.runTransaction({ (transaction, errorPointer) -> Any? in
+            let businessDocument = self.db.collection("businesses").document(event.businessId)
+            let businessSnapshot: DocumentSnapshot
+            
+            do {
+                try businessSnapshot = transaction.getDocument(businessDocument)
+            } catch let fetchError as NSError {
+                errorPointer?.pointee = fetchError
+                return nil
+            }
+            
+            var business: Business
+            
+            if let oldBusinessData = businessSnapshot.data() {
+                business = try! FirestoreDecoder().decode(Business.self, from: oldBusinessData)
+            } else {
+                business = Business()
+            }
+            
+            switch event.type {
+            case .enter:
+                business.activeCustomerCount += 1
+            case .exit:
+                business.activeCustomerCount -= 1
+            }
+            
+            let newBusinessData = try! FirestoreEncoder().encode(business)
+            transaction.setData(newBusinessData, forDocument: businessDocument)
+
+            let eventData = try! FirestoreEncoder().encode(event)
+            let eventDocument = self.db.collection("events").document()
+            transaction.setData(eventData, forDocument: eventDocument)
+            
+            return nil
+        }, completion: completion)
+    }
+    
+    private lazy var db = Firestore.firestore()
 }
 
 
