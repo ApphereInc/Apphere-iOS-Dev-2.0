7f1cbb03a1e91511b15c9f101642ef746cc7af32
Tony Mann <thephatmann@gmail.com>
2018-03-13 11:00:56 -0700

    Featured pages

--- apphere/SecondViewController.swift
+++ apphere/SecondViewController.swift
@@ -9,20 +9,53 @@
 import UIKit
 import WebKit
 
-class SecondViewController: UIViewController {
-    
-    @IBOutlet var webView: WKWebView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let url = URL(string: "https://google.com")
-        webView.load(URLRequest(url: url!))
-        
-        
-        
+class SecondViewController: UIViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "pageView" {
+            pageControllers = [
+                storyboard!.instantiateViewController(withIdentifier: "featured1"),
+                storyboard!.instantiateViewController(withIdentifier: "featured2")
+            ]
+
+            let pageViewController = segue.destination as! UIPageViewController
+            pageViewController.dataSource = self
+            pageViewController.delegate = self
+            pageViewController.setViewControllers([pageControllers[0]], direction: .forward, animated: false, completion: nil)
+        }
     }
     
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = pageControllers.index(of: viewController) else {
+            preconditionFailure()
+        }
+        
+        if index == 0 {
+            return nil
+        }
+        
+        return pageControllers[index - 1]
+    }
     
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = pageControllers.index(of: viewController) else {
+            preconditionFailure()
+        }
+        
+        if index == pageControllers.endIndex - 1 {
+            return nil
+        }
+        
+        return pageControllers[index + 1]
+    }
     
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pageControllers.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        return 0
+    }
+    
+    var pageControllers: [UIViewController]!
 }
 
