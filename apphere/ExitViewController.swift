579cae7e6687e890c0586508bfa5fe37a05bbe45
Tony Mann <thephatmann@gmail.com>
2018-03-19 14:56:49 -0700

    ExitViewController

---
+++ apphere/ExitViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ExitViewController.swift
+//  apphere
+//
+//  Created by Tony Mann on 3/19/18.
+//  Copyright © 2018 Derek Sheldon. All rights reserved.
+//
+
+import UIKit
+
+class ExitViewController: UIViewController {
+
+}
