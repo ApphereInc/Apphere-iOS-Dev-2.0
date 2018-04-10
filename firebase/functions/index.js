492b77b43cf2ee9bff32bacd40350d8c8035129c
Tony Mann <thephatmann@gmail.com>
2018-04-10 16:10:14 -0700

    Cloud function to auto-exit deployed

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -6,9 +6,28 @@
 //  Copyright © 2018 Derek Sheldon. All rights reserved.
 //
 
+const maxMillisecondsBeforeExit = 60 * 60 * 1000
+
 var functions = require('firebase-functions');
+var admin = require('firebase-admin');
+
+admin.initializeApp();
 
 exports.hourly_job =
   functions.pubsub.topic('hourly-tick').onPublish((event) => {
-    console.log("TODO: exit active customers that have been in a business over 15 minutes")
+    
+    var maxEnterDate = new Date();
+    maxEnterDate.setTime(maxEnterDate.getTime() - maxMillisecondsBeforeExit)
+
+    var firestore = admin.firestore();
+
+    return firestore.collection('customers')
+      .where('exit_date', '==', null)
+      .where('enter_date', '<', maxEnterDate)
+      .get().then(querySnapshot => {
+        querySnapshot.forEach(customerSnapshot => {
+          console.log("Exiting customer " + customerSnapshot.get("user_id") + " at business " + customerSnapshot.get("business_id"))
+          customerSnapshot.ref.update({exit_date: new Date()})
+        });
+      });
   });
